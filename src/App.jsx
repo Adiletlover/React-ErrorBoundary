675a4dae272337296f203d641febea939f2d3498
Adiletlover <adilet.price@gmail.com>
2023-03-15 18:07:19 +0600

    hw5

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect(() => {
+    throw new Error()
+  }, [])
+
+  return (
+    <h1>hello world</h1>
+  )
+}
+
+export default App;
